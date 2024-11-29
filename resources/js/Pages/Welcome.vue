ea117280d57b8e674f1f6c08b8edec0d3dc6df41
AJ DDcatoria <ajdedicatoria9@gmail.com>
2024-11-29 18:35:28 +0800

    initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div class="h-dvh flex items-center justify-center">
+        <h1 class="text-4xl font-bold">Welcome to Vue.js</h1>
+    </div>
+</template>
